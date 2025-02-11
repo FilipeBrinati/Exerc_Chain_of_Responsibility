e468e3d5378c6559cf199492c0f61dacaa4b94fa
FilipeBrinati <44785517+FilipeBrinati@users.noreply.github.com>
2025-02-11 19:58:59 -0300

    bug fixes

--- AverageBusiness/src/test/java/chain/COFTests.java
+++ AverageBusiness/src/test/java/chain/COFTests.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class ChainOfResponsibilityTest {
+class COFTests {
 
     private Funcionario funcionarioComum;
     private Funcionario gerente;
